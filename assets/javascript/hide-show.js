938ef81956517cdba2c763a232aa55402d372a32
alexelash <alexelash@users.noreply.github.com>
2021-05-28 10:22:45 +0000

    jekyll build from Action 2da54455e40859f78e3e8a6017b0e7130c53a96d

---
+++ assets/javascript/hide-show.js
@@ -0,0 +1,14 @@
+(function() {
+    const toggle = document.querySelectorAll('.-js-toggle')
+
+    Array.prototype.forEach.call(toggle, toggle => {
+        let target = toggle.parentElement.nextElementSibling
+
+        toggle.onclick = () => {
+            let expanded = toggle.getAttribute('aria-expanded') === 'true' || false
+
+            toggle.setAttribute('aria-expanded', !expanded)
+            target.hidden = expanded    
+        }
+    })
+})()
